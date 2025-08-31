b7970baa50ebf0e87355c54bf565410cbf7e437c
Rob Herley <robherley13@gmail.com>
2025-08-30 22:15:50 -0400

    cleanup example

--- examples/basic.js
+++ examples/basic.js
@@ -1,12 +1,5 @@
-const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export async function handler(req) {
-  // console.log("Request:", req);
   const who = req.body ? JSON.parse(req.body).name : "World";
-
-  await wait(20000);
-  // while (true) {}
-
   return {
     status: 200,
     headers: {
